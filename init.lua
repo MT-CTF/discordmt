1c81ca5ecc84f6fb81852352312ab2ac98f11e3c
LandarVargan <lonewolf04361@gmail.com>
2026-01-10 22:12:26 -0500

    Allow configuring the host (p.2) (#7)
    * Update settingtypes.txt
    
    * Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 local http = minetest.request_http_api()
 local settings = minetest.settings
 
+local host = settings:get('discord.host') or "localhost"
 local port = settings:get('discord.port') or 8080
 local timeout = 10
 
@@ -94,7 +95,7 @@ function discord.handle_response(response)
                 success = result
             }
             http.fetch({
-                url = 'localhost:'..tostring(port),
+                url = host..':'..tostring(port),
                 timeout = timeout,
                 post_data = minetest.write_json(request)
             }, discord.handle_response)
@@ -111,7 +112,7 @@ function discord.send(message, id)
         data['context'] = id
     end
     http.fetch_async({
-        url = 'localhost:'..tostring(port),
+        url = host..':'..tostring(port),
         timeout = timeout,
         post_data = minetest.write_json(data)
     })
@@ -148,7 +149,7 @@ minetest.register_globalstep(function(dtime)
                 if res.completed == true then
                     discord.handle_response(res)
                     ongoing = http.fetch_async({
-                        url = 'localhost:'..tostring(port),
+                        url = host..':'..tostring(port),
                         timeout = timeout,
                         post_data = minetest.write_json({
                             type = 'DISCORD-REQUEST-DATA'
